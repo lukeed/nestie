0d3a3949fecf8ee3d5bf132317f6ac93bf2560e1
Luke Edwards <luke.edwards05@gmail.com>
2020-09-08 16:39:28 -0700

    fix: separate elifs

--- src/index.js
+++ src/index.js
@@ -6,20 +6,23 @@ export function nestie(input, glue) {
 	for (k in input) {
 		tmp = output; // reset
 		arr = k.split(glue);
+
 		for (i=0; i < arr.length;) {
 			key = arr[i++];
 			if (tmp == null) {
 				char = (''+key).charCodeAt(0);
 				tmp = (char > 47 && char < 58) ? [] : {};
 				output = output || tmp;
-			} else if (i < arr.length) {
+			}
+
+			if (i < arr.length) {
 				if (key in tmp) {
 					tmp = tmp[key];
 				} else {
 					char = (''+arr[i]).charCodeAt(0);
 					tmp = tmp[key] = (char > 47 && char < 58) ? [] : {};
 				}
-			} else if (i === arr.length) {
+			} else {
 				tmp[key] = input[k];
 			}
 		}
